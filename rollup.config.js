4ad9f62ffcf8ac8391514c57dbb4618e0ce9cb25
Carl Chen <ccheartszzz@gmail.com>
2024-06-09 23:44:10 +0800

    chore: support cjs require

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import typescript from "rollup-plugin-typescript2";
-// import tsConfig from "./tsconfig.json" assert { type: "json" };
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { readFile } from "fs/promises";
@@ -12,9 +11,15 @@ tsConfig = JSON.parse(tsConfig);
 console.log(tsConfig);
 export default {
   input: "./lib/index.ts",
-  output: {
-    file: "dist/index.js",
-    format: "esm",
-  },
+  output: [
+    {
+      file: "dist/index.js",
+      format: "esm",
+    },
+    {
+      file: "dist/index.cjs",
+      format: "cjs",
+    }
+  ],
   plugins: [resolve(), commonjs(), typescript(tsConfig)],
 };
